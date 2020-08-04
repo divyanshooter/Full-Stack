e5476fca07a9e011583c339ccc8c3eaeef8dedc4
divyanshooter <divyanshooter@gmail.com>
2020-08-04 23:55:57 +0530

    Index and aboutus page added and served by the server

--- node-practice/restaurant/index.js
+++ node-practice/restaurant/index.js
@@ -1,16 +1,47 @@
 const http=require('http');
-const { hostname } = require('os');
+const path=require('path');
+const fs=require('fs');
 
-const host='localhoast';
+const host='localhost';
 const port=8080;
 
 const server=http.createServer((req,res)=>{
-    console.log(req.headers);
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>Hello World! </h1></body></html>');
+     if(req.method=='GET') {
+         var fileUrl;
+         if(req.url=='/') fileUrl='/index.html';
+         else fileUrl=req.url;
+
+         var filepath=path.resolve('./public'+ fileUrl);
+         const fileext=path.extname(filepath);
+         if(fileext=='.html') {
+             fs.exists(filepath,exist=>{
+                 if(!exist) {
+                     res.statusCode=404;
+                     res.setHeader('Content-Type','text/html');
+                     res.end('<html><body>NOt Found</body></html');
+                     return;
+                 }
+                 res.statusCode=200;
+                 res.setHeader('Content-Type','text/html');
+                 fs.createReadStream(filepath).pipe(res);
+             });
+
+         }
+         else {
+            res.statusCode=404;
+            res.setHeader('Content-Type','text/html');
+            res.end('<html><body>NOt an HTML file</body></html');
+            return;
+         }
+     }
+     else {
+        res.statusCode=404;
+        res.setHeader('Content-Type','text/html');
+        res.end('<html><body>Method not supported</body></hmtl');
+        return;
+     }
 });
 
-server.listen(port,hostname,()=>{
-    console.log(`Server running at http://${hostname}:${port}`);
+server.listen(port,host,()=>{
+    console.log(`Server running at http://${host}:${port}`);
 })
