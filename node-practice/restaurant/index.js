f5f059fd6dea829c36d92ee2f5f8fd108065b2bf
divyanshooter <divyanshooter@gmail.com>
2020-08-04 23:58:45 +0530

    Error handling added

--- node-practice/restaurant/index.js
+++ node-practice/restaurant/index.js
@@ -18,7 +18,8 @@ const server=http.createServer((req,res)=>{
                  if(!exist) {
                      res.statusCode=404;
                      res.setHeader('Content-Type','text/html');
-                     res.end('<html><body>NOt Found</body></html');
+                     res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                     ' not found</h1></body></html>');
                      return;
                  }
                  res.statusCode=200;
@@ -30,14 +31,16 @@ const server=http.createServer((req,res)=>{
          else {
             res.statusCode=404;
             res.setHeader('Content-Type','text/html');
-            res.end('<html><body>NOt an HTML file</body></html');
+            res.end('<html><body><h1>Error 404: ' + fileUrl + 
+              ' not a HTML file</h1></body></html>');
             return;
          }
      }
      else {
         res.statusCode=404;
         res.setHeader('Content-Type','text/html');
-        res.end('<html><body>Method not supported</body></hmtl');
+        res.end('<html><body><h1>Error 404: ' + req.method + 
+              ' not supported</h1></body></html>');
         return;
      }
 });
