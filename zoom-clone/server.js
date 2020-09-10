2d2fb4f28b1101d8eaa5a185c968f426d80e76cd
divyanshooter <divyanshooter@gmail.com>
2020-09-11 00:50:54 +0530

    personal video added

--- zoom-clone/server.js
+++ zoom-clone/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const server = require("http").Server(app);
 
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
 });
 server.listen(3030);
